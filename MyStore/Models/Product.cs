66c0050a52f761d3cd97530873fe3017922da0aa
TuanMCIk <mcfc2k1@gmail.com>
2022-11-25 23:24:59 +0700

    click

--- MyStore/Models/Product.cs
+++ MyStore/Models/Product.cs
@@ -1,7 +1,8 @@
-namespace MyStore.Models
+﻿namespace MyStore.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -10,35 +11,49 @@ namespace MyStore.Models
     public partial class Product
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required(ErrorMessage = "Mã sản phẩm không được để trống !")]
+        [DisplayName("Mã sản phẩm")]
         public int ProductID { get; set; }
 
-        [Required]
+
+        [Required(ErrorMessage = "Tên sản phẩm không được để trống !")]
+        [DisplayName("Tên sản phẩm")]
         [StringLength(50)]
         public string ProductName { get; set; }
 
+        [Required(ErrorMessage = "Danh mục không được để trống !")]
+
         [Column(TypeName = "text")]
+        [DisplayName("Mô tả")]
         public string Description { get; set; }
 
         [Column(TypeName = "numeric")]
+        [DisplayName("Giá nhập")]
         public decimal PurchasePrice { get; set; }
 
         [Column(TypeName = "numeric")]
+        [DisplayName("Giá bán")]
         public decimal Price { get; set; }
 
+        [DisplayName("Số lượng")]
         public int Quantity { get; set; }
 
         [StringLength(20)]
+        [DisplayName("Năm sản xuất")]
         public string Vintage { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Danh mục không được để trống !")]
         [StringLength(10)]
+        [DisplayName("Danh mục")]
         public string CatalogyID { get; set; }
 
         [Column(TypeName = "text")]
+        [DisplayName("Hình ảnh")]
         public string Image { get; set; }
 
-        [Required]
+      
         [StringLength(100)]
+        [DisplayName("Xuất xứ")]
         public string Region { get; set; }
 
         public virtual Catalogy Catalogy { get; set; }
